e155b036cb77253912301310b8dbb727553d6eb7
vkrovi <vkrovi@islander.tamucc.edu>
2023-12-28 17:28:21 -0600

    initial commit

---
+++ src/com/Booking/Booking.java
@@ -0,0 +1,13 @@
+package com.Booking;
+
+public class Booking {
+    public static void main(String [] args){
+        System.out.println("Hotel Booking Management");
+        System.out.println("Please select your preferred choice");
+        System.out.println("1. Register account");
+        System.out.println("2. Book reservation");
+        System.out.println("3. Generate bill");
+        System.out.println("4. Feedback");
+
+    }
+}
